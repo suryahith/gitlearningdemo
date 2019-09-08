bb1f66eee7f8b22d6b4270a1b5c030bf64c9bef4
shinnymol <surya.hith@outlook.com>
2019-09-08 09:34:20 -0400

    first commit

---
+++ src/methodoverloading/staticmethodmain1.java
@@ -0,0 +1,21 @@
+package methodoverloading;
+
+public class staticmethodmain1 {
+
+	public static void main(String[] args) {
+	
+			// TODO Auto-generated method stub
+
+			staticmethodmain s1=new staticmethodmain();
+			s1.a=123;
+			staticmethodmain.b=30;
+			
+			staticmethodmain.statictest();
+			s1.test();
+		}
+
+		// TODO Auto-generated method stub
+
+	}
+
+
